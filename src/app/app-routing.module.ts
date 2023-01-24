07139aefc644d6cfdb48e0f030425735405f6b4c
Dan Moulton <dan.moulton@icloud.com>
2023-01-24 02:33:40 +0000

    feat(routing): added routing to core module and base components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,64 @@
-import { RouterModule, Routes } from '@angular/router';
-import { NgModule } from '@angular/core';
+import { Injectable, NgModule } from '@angular/core';
+import { RouterModule, RouterStateSnapshot, Routes, TitleStrategy } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
-const routes: Routes = [];
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { ExperienceComponent } from './experience/experience.component';
+
+const routes: Routes = [
+    {
+        path: 'about',
+        component: AboutComponent,
+        title: 'About'
+    },
+    {
+        path: 'experience',
+        component: ExperienceComponent,
+        title: 'Experience'
+    },
+    {
+        path: 'blog',
+        component: BlogComponent,
+        title: 'Blog'
+    },
+    {
+        path: 'contact',
+        component: ContactComponent,
+        title: 'Contact'
+    },
+    {
+        path: '',
+        redirectTo: '/about',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        component: AboutComponent
+    }
+];
+
+@Injectable({ providedIn: 'root' })
+export class TemplatePageTitleStrategy extends TitleStrategy {
+    constructor(private readonly title: Title) {
+        super();
+    }
+
+    override updateTitle(routerState: RouterStateSnapshot) {
+        const title = this.buildTitle(routerState);
+
+        if (title !== undefined) {
+            this.title.setTitle(`Dan M | ${title}`);
+        }
+    }
+}
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+    exports: [RouterModule],
+    providers: [
+        { provide: TitleStrategy, useClass: TemplatePageTitleStrategy },
+    ]
 })
 export class AppRoutingModule { }
