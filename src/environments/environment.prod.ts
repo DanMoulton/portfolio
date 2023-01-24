efe56b1edaa42aad4ba3d5465dde0f8dcebac758
Dan Moulton <dan.moulton@icloud.com>
2023-01-24 02:31:54 +0000

    feat(mobile-nav): implemented nav menu for mobile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+    production: true,
+
+    author: 'Dan Moulton',
+    githubUrl: 'https://github.com/DanMoulton',
+    linkedInUrl: 'https://www.linkedin.com/in/dan-moulton18/'
 };
