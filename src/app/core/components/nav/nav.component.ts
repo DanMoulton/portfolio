b423a62c7b220fe7f11bcd0061e4ea209f09347c
Dan Moulton <dan.moulton@icloud.com>
2023-01-24 02:31:54 +0000

    feat(mobile-nav): implemented nav menu for mobile

--- src/app/core/components/nav/nav.component.ts
+++ src/app/core/components/nav/nav.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, Routes } from '@angular/router';
+
+import { environment } from '../../../../environments/environment';
 
 @Component({
     selector: 'app-nav',
     templateUrl: './nav.component.html',
     styleUrls: ['./nav.component.scss']
 })
-export class NavComponent {
-    public navToggle = false;
+export class NavComponent implements OnInit {
+    public environment = environment;
+    public navOpen = false;
+    public routes: Routes = [];
+
+    constructor(private router: Router) { }
+
+    ngOnInit() {
+        this.getRoutes(this.router.config);
+    }
+
+    private getRoutes(routes: Routes): void {
+        for (let i = 0; i < routes.length; i++) {
+            if (routes[i].path && !routes[i]?.path?.startsWith('*')) {
+                this.routes.push(routes[i]);
+            }
+        }
+    }
 }
