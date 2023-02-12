94d2c5a8e196af6ebd78a8e494a667a45ccbf6ab
Dan Moulton <dan.moulton@icloud.com>
2023-02-12 22:23:34 +0000

    feat(particles): added ParticlesJS library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import ParticlesConfig from './../assets/scripts/particles/particles.json';
+
+declare let particlesJS: any;
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+    ngOnInit(): void {
+        particlesJS('particles-js', ParticlesConfig, null);
+    }
+}
