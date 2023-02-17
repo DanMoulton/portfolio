fd2bab2abefaa713f8bcb8dc20e72df121de12d8
Dan Moulton <dan.moulton@icloud.com>
2023-02-17 05:16:16 +0000

    feat(social): added link to stack-overflow profile

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@ export const environment = {
 
     author: 'Dan Moulton',
     githubUrl: 'https://github.com/DanMoulton',
+    stackOverflowUrl: 'https://stackoverflow.com/users/4077878/dan-moulton',
     linkedInUrl: 'https://www.linkedin.com/in/dan-moulton18/',
     romURL: 'https://ia801506.us.archive.org/cors_get.php?path=/26/items/tetris-gb/Tetris.gb'
 };
