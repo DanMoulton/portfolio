db40c099a2188508b266d5108ba13d19c9f04674
Dan Moulton <dan.moulton@icloud.com>
2023-03-06 04:16:35 +0000

    feat(splash-screen): implemented splash screen with brand logo animation

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,29 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { IfScreenSizeDirective } from './directives/if-screen-size.directive';
 import { NavComponent } from './components/nav/nav.component';
-import { RouterModule } from '@angular/router';
+import { SplashScreenComponent } from './components/splash-screen/splash-screen.component';
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    FooterComponent,
-    NavComponent,
-    IfScreenSizeDirective
-  ],
-  imports: [
-    CommonModule,
-    RouterModule
-  ],
-  exports: [
-    RouterModule,
-    HeaderComponent,
-    FooterComponent,
-    NavComponent,
-    IfScreenSizeDirective
-  ]
+    declarations: [
+        FooterComponent,
+        HeaderComponent,
+        NavComponent,
+        SplashScreenComponent,
+        IfScreenSizeDirective
+    ],
+    imports: [
+        CommonModule,
+        RouterModule
+    ],
+    exports: [
+        RouterModule,
+        FooterComponent,
+        HeaderComponent,
+        NavComponent,
+        SplashScreenComponent,
+        IfScreenSizeDirective
+    ]
 })
 export class CoreModule { }
