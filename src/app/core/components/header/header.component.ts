c4b4aca692e25d74f0df80e7e42b946b80a9d694
Dan Moulton <dan.moulton@icloud.com>
2023-03-20 02:06:38 +0000

    feat(header): implementation of header for medium and large screens

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, Routes } from '@angular/router';
+
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+    public environment = environment;
+
+    public routes: Routes = [];
+
+    constructor(private router: Router) { }
+
+    ngOnInit(): void {
+        this.getRoutes(this.router.config);
+    }
+
+    private getRoutes(routes: Routes): void {
+        for (let i = 0; i < routes.length; i++) {
+            if (routes[i].path && !routes[i]?.path?.startsWith('*')) {
+                this.routes.push(routes[i]);
+            }
+        }
+    }
 
 }
